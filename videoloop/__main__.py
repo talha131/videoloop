8e1a0bf1ec4ef5936aa57fec297c794128d7ea0f
Talha Mansoor <talha131@gmail.com>
2018-10-07 09:39:14 +0500

    Add type checking for time argument

--- videoloop/__main__.py
+++ videoloop/__main__.py
@@ -1,12 +1,21 @@
 import argparse
 
+
+def check_positive(value):
+    ivalue = int(value)
+    if ivalue <= 0:
+        raise argparse.ArgumentTypeError(
+            "Positive number is required. %s is invalid" % value)
+    return ivalue
+
+
 parser = argparse.ArgumentParser(
     description='Create loops of a video. Clips are overlapped with fade transition.')
 parser.add_argument('file', metavar='FILENAME',
                     help='Video file whose loop is required')
 
 requiredNamed = parser.add_argument_group('required arguments')
-requiredNamed.add_argument('-t', '--time', metavar='MINUTES', required=True,
+requiredNamed.add_argument('-t', '--time', metavar='MINUTES', required=True, type=check_positive,
                            help='Required duration of the video loop')
 
 
