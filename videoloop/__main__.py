2cd6ae9dd1cb9a4b9f103cf9a83266d0d98344c8
Talha Mansoor <talha131@gmail.com>
2018-10-07 09:10:36 +0500

    Add parser for commandline

--- videoloop/__main__.py
+++ videoloop/__main__.py
@@ -1,5 +1,16 @@
+import argparse
+
+parser = argparse.ArgumentParser(
+    description='Create loops of a video. Clips are overlapped with fade transition.')
+parser.add_argument('file', metavar='FILENAME',
+                    help='Video file whose loop is required')
+parser.add_argument('-t', '--time', metavar='MINUTES', required=True,
+                    help='Required duration of the video loop')
+
+
 def main():
-    print('in main')
+    args = parser.parse_args()
+    print(args)
 
 
 if __name__ == '__main__':
