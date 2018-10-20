3fdb7d73406dbff7ca5497c01b10392d1bfbd203
Talha Mansoor <talha131@gmail.com>
2018-10-20 15:29:01 +0500

    Exit program if required duration is less than clip duration

--- videoloop/__main__.py
+++ videoloop/__main__.py
@@ -29,6 +29,11 @@ def main():
     clip_d = clip_duration(args.file.name)
     if clip_d <= args.duration:
         sys.exit('Transition duration must be less than clip duration')
+    if clip_d <= args.time:
+        print('Required duration must be greater than clip duration')
+        print('Required: {0} {2}\nClip: {1} {2}'.format(
+            args.time * 60, clip_d, 'seconds'))
+        sys.exit(1)
 
 
 if __name__ == '__main__':
